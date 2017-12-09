8cba0f745adabfc93ba0fbd7845ace28c25414c5
Shayan-Ahmed <34052369+Shayan-Ahmed@users.noreply.github.com>
2017-12-09 15:40:22 -0800

    Add files via upload
    The following is the java bean classes for Valet and WakeUpCall

---
+++ model/WakeUpCall.java
@@ -0,0 +1,29 @@
+
+public class WakeUpCall 
+{
+	private int gID;
+	private String time;
+	
+	public WakeUpCall(int gID, String time)
+	{
+		this.gID = gID;
+		this.time = time;
+	}
+
+	public int getgID() {
+		return gID;
+	}
+
+	public void setgID(int gID) {
+		this.gID = gID;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
