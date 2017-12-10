6a37c3d957805939d4140c7d1d5e5405e6a61d31
Rishabh3095 <rishabh.gupta@sjsu.edu>
2017-12-09 17:38:35 -0800

    updated

--- controller/Queries.java
+++ controller/Queries.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -10,6 +11,7 @@ import model.Employee;
 import model.Guest;
 import model.Payment;
 import model.Room;
+import util.ConnectDB;
 
 public class Queries {
 	
@@ -43,68 +45,68 @@ public class Queries {
 		    }
 		    return result;
 	  }
-	  
-	  public static int employerLogin(Employee em) throws SQLException
+	  	  
+	  public static void employerLogin() throws SQLException
 	  {
+		  System.out.println("Please enter the employer ID: ");
+		  
+		  Scanner ini = new Scanner(System.in);
+		  int eId = ini.nextInt();
+		  
+		  System.out.println("Please enter the password: ");
+		  String pass = ini.next();
+		  
 		  PreparedStatement pstmt = null;
-		    int result = 0;
+		  ResultSet result = null;
+		  
 		    try {
 		        String signIn =
-		                "SELECT eId, Password from EMPLOYEE where EiD = (?) and Password = (?);";
+		                "SELECT eId, Password from EMPLOYEE where eId = (?) and Password = (?);";
 
 		      pstmt = connection.prepareStatement(signIn);
-		      pstmt.setInt(1, em.geteId());
-		      pstmt.setString(2, em.getPass());
-		      result = pstmt.executeUpdate();
-
+		      pstmt.setInt(1, eId);
+		      pstmt.setString(2, pass);
+		      result = pstmt.executeQuery();
+		      
+		      while (result.next()) {
+		          int eI = result.getInt("eId");
+		          String pas = result.getString("Password");
+		          System.out.print(eI + " " + pas + " ");
+		          System.out.println(" ");
+		        }
 		    } catch (SQLException e) {
 		      e.printStackTrace();
 		    } finally {
 		      pstmt.close();
 		    }
-		    return result;
 	  }
 	  
-	  /*
-	  public static int userLogin(Guest gst) throws SQLException
+	  public static void averageRoomPrice() throws SQLException
 	  {
 		  PreparedStatement pstmt = null;
-		    int result = 0;
-		    try {
-		        String signIn =
-		                "SELECT rId, Password from GUEST where rId = (?) and Password = (?);";
-
-		      pstmt = connection.prepareStatement(signIn);	      
-		      pstmt.setInt(1, gst.getrId());
-		      pstmt.setString(2, gst.get);
-		      result = pstmt.executeUpdate();
-
-		    } catch (SQLException e) {
-		      e.printStackTrace();
-		    } finally {
-		      pstmt.close();
-		    }
-		    return result;
-	  }
-	  */
-
-	  public static int averageRoomPrice(Room rm) throws SQLException
-	  {
-		  PreparedStatement pstmt = null;
-		    int result = 0;
+		    ResultSet result = null;
 		    try {
 		        String avgPrice =
 		                "SELECT roomNum, Type, price, smoke from Room R1 where price > (SELECT avg(price) from Room where R1.type = type) and rId in (SELECT rID from Reservations);";
 
 		      pstmt = connection.prepareStatement(avgPrice);	      
-		      result = pstmt.executeUpdate();
+		      result = pstmt.executeQuery();
 
+		      while (result.next()) {
+		          int roomNum = result.getInt("roomNum");
+		          String Type = result.getString("Type");
+		          Float pric = result.getFloat("price");
+		          boolean bl = result.getBoolean("smoke");
+		          System.out.print(roomNum + " " + Type + " " + " " + pric + " " + bl);
+		          System.out.println(" ");
+		        }
+
+		      
 		    } catch (SQLException e) {
 		      e.printStackTrace();
 		    } finally {
 		      pstmt.close();
 		    }
-		    return result;
 	  }
 	  
 	  public static int markRoomUnav(Room rm) throws SQLException {
@@ -112,7 +114,7 @@ public class Queries {
 		    int result = 0;
 		    try {
 		      String roomInfo =
-		          "\"INSERT INTO UNROOM (rId) VALUE (?);";
+		          "INSERT INTO UNROOM (rId) VALUE (?);";
 
 		      pstmt = connection.prepareStatement(roomInfo);
 		      pstmt.setInt(1, rm.getrId());
@@ -126,49 +128,31 @@ public class Queries {
 		    return result;
 	  }
 	  
-	  public static int guestInfo(Guest guest) throws SQLException {
+	  public static void guestInfo() throws SQLException {
 		  
 		    PreparedStatement pstmt = null;
-		    int result = 0;
+		    ResultSet result = null;
 		    try {
 		      String guestInfo =
 		          "Select (*) from (Payment JOIN Guest);";
 
 		      pstmt = connection.prepareStatement(guestInfo);
-		      result = pstmt.executeUpdate();
+		      result = pstmt.executeQuery();
+		      
+		      while (result.next()) {
+		    	  int gid = result.getInt("gId");
+		    	  String name = result.getString("Name");
+		    	  int cardnumber = result.getInt("CardNumber");
+		    	  int expiration = result.getInt("Expiration");
+		          System.out.print(gid + " " + name + " " + " " + cardnumber+ " " + expiration);
+		          System.out.println(" ");
+		        }
+
 
 		    } catch (SQLException e) {
 		      e.printStackTrace();
 		    } finally {
 		      pstmt.close();
 		    }
-		    return result;
 		  }
-	  
-	  /*
-	   * Display all the room details whose price is greater than average price for that type and is currently reserved. (ADMIN : for SALES purposes)
-	   */
-	  public static int addGuest(Guest guest) throws SQLException {
-		    PreparedStatement pstmt = null;
-		    int result = 0;
-		    try {
-		      String addGuest =
-		          "INSERT INTO Guest (rId, Name, Address, Email, checkIn, checkOut) VALUES (? , ?, ?, ?, ?, ?);";
-
-		      pstmt = connection.prepareStatement(addGuest);
-		      pstmt.setInt(1, guest.getrId());
-		      pstmt.setString(2, guest.getName());
-		      pstmt.setString(3, guest.getAddress());
-		      pstmt.setString(4, guest.getEmail());
-		      pstmt.setDate(5, guest.getCheckIn());
-		      pstmt.setDate(6, guest.getCheckOut());
-		      result = pstmt.executeUpdate();
-
-		    } catch (SQLException e) {
-		      e.printStackTrace();
-		    } finally {
-		      pstmt.close();
-		    }
-		    return result;
-		  }
-}
+	}
